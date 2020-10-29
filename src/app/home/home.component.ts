26eb4a3496251b8a2da54729023934d5723481c6
Osama Bisheer <osamamohammed9694@gmail.com>
2020-10-30 00:06:40 +0200

    fg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCoffee, faHome, faDollarSign, faBook, faUser, faChartPie, faPlus, faDownload, faAngleDoubleLeft,
+  faCog, faLink, faBars, faTimes, faCaretDown, faSearch, faAngleUp, faAngleDown, faAngleDoubleRight
+} from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faLinkedinIn, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faQuestionCircle, faEdit } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +12,30 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  faTimes = faTimes;
   faCoffee = faCoffee;
+  faHome = faHome;
+  faDollarSign = faDollarSign;
+  faBook = faBook;
+  faUser = faUser;
+  faLink = faLink;
+  faChartPie = faChartPie;
+  faCog = faCog;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedinIn;
+  faFacebook = faFacebookF;
+  faBars = faBars;
+  faEnvelope = faEnvelope;
+  faQuestionCircle = faQuestionCircle;
+  faCaretDown = faCaretDown;
+  faSearch = faSearch;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faPlus = faPlus;
+  faDownload = faDownload;
+  faEdit = faEdit;
+  faAngleDoubleLeft = faAngleDoubleLeft;
+  faAngleDoubleRight = faAngleDoubleRight;
 
   constructor() { }
 
